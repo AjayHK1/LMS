df3b5eab92d9f55cff6b7dfdce4688319f82d2eb
AjayHK1 <ajay.l1252@gmail.com>
2016-12-03 07:35:10 +0530

    Add files via upload

---
+++ DA/Abstract/ISupplierMasterDA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DA.Abstract
+{
+    public interface ISupplierMasterDA
+    {
+       int RetrieveSupplierIDFromDB();
+       int saveSupplierDetailsToDB(SupplierMaster supplier);
+       int updateSupplierDetailsToDB(SupplierMaster supplier);
+       int deleteSupplierDetailsFromDB(int supplierId);
+       DataTable GetSupplierDetailsFromDBToGrid();
+       DataTable GetSupplierDetailsFromDBToGrid(int Id);
+    }
+}
